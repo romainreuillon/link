3ec44e7d4f79f60dbb0167ae55018622164d7140
Romain Reuillon <romain.reuillon@iscpif.fr>
2017-07-16 21:20:04 +0200

    Perform transaction, support metamask and uport connect.

--- src/main/scala/Link.scala
+++ src/main/scala/Link.scala
@@ -19,13 +19,14 @@
 package vote.webapp
 
 import scala.scalajs.js
+import scala.scalajs.js._
 import org.scalajs.dom
 
 import scalatags.JsDom.all._
 import rx._
 import RxWrap._
 
-import scala.scalajs.js.annotation.JSGlobal
+import scala.scalajs.js.annotation.{JSGlobal, JSName}
 
 object Link extends js.JSApp {
 //
@@ -41,6 +42,9 @@ object Link extends js.JSApp {
 //    def getAddressString(): String = js.native
 //    def getPrivateKey(): js.Array[Int] = js.native
 //  }
+
+
+  type CallBack[T] = js.Function2[js.Error, T, _]
 
   @js.native
   trait HttpProvider extends js.Object {
@@ -61,8 +65,11 @@ object Link extends js.JSApp {
 
   @js.native
   trait Eth extends js.Object {
-    def getBalance(address: String): BigNumber
+    def getBalance(address: String, defaultBlock: String | Number = js.native, callBack: CallBack[BigNumber] = js.native): BigNumber
     def contract(abi: js.Dynamic): js.Dynamic
+    def accounts: js.Dynamic
+    def getAccounts(callBack: CallBack[js.Array[js.Dynamic]])
+    def sendTransaction(transactionObject: js.Dictionary[js.Any], callBack: CallBack[String] = js.native): String
   }
 
   @js.native
@@ -72,116 +79,198 @@ object Link extends js.JSApp {
   }
 
 
-
-    def main(): Unit = {
-
-    val contractABI =
-      js.JSON.parse("""[{"constant":false,"inputs":[],"name":"getNb","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"propose","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"inputs":[],"payable":false,"type":"constructor"}]""")
-
-
-    val balanceValue = Var { "NA"}
-    val contactValue = Var { "NA" }
-
-
-    //    js.Dynamic.global.requirejs(
-//      js.Array("./ethereumjs-tx"),
-//      (ns: js.Dynamic) => {
-//        val rawTx = js.Array(
-//          "0x00",
-//          "0x09184e72a000",
-//          "0x2710",
-//          "0x0000000000000000000000000000000000000000",
-//          "0x00",
-//          "0x7f7465737432000000000000000000000000000000000000000000000000000000600057",
-//          "0x1c",
-//          "0x5e1d3a76fbf824220eafc8c79ad578ad2b67d01b0c2425eb1f1347e8f50882ab",
-//          "0x5bd428537f05f9830e93792f90ea6a3e2d1ee84952dd96edbae9f658f831ab13")
-//        val tx = js.Dynamic.newInstance(ns.Tx)(rawTx).asInstanceOf[Transaction]
-//        println(tx.verifySignature())
-//      }
-//    )
-
-//    def load(wallet: String, password: String) = {
-//      println(js.typeOf(js.Dynamic.global.mist))
-//      js.Dynamic.global.requirejs(
-//        js.Array("./ethereumjs-wallet", "ethereumjs-tx"),
-//        (nsW: js.Dynamic, nsTx: js.Dynamic) => {
-//          val w = nsW.Wallet.fromV3(wallet, password).asInstanceOf[Wallet]
-//
-//          val rawTx = js.Dictionary(
-//             "nonce" -> "0x00",
-//             "gasPrice" -> "0x09184e72a000",
-//             "gasLimit" -> "0x2710",
-//             "to" -> "0x0000000000000000000000000000000000000000",
-//             "value" -> "0x00",
-//             "data" -> "0x7f7465737432000000000000000000000000000000000000000000000000000000600057"
-//          )
-//
-//          val tx = js.Dynamic.newInstance(nsTx.Tx)(rawTx).asInstanceOf[Transaction]
-//          tx.sign(w.getPrivateKey)
-//
-//          println(tx.serialize().mkString)
-//         // v() = w.getPrivateKey()
-//        }
-//      )
-//    }
-
-
-    def web3 = {
-      val web3 = js.Dynamic.newInstance(js.Dynamic.global.Web3)().asInstanceOf[Web3]
-      val provider = js.Dynamic.newInstance(web3.providers.HttpProvider)("http://localhost:8545").asInstanceOf[HttpProvider]
-      web3.setProvider(provider)
-      web3
-    }
-
-    def queryBalance = () => {
-//     js.Dynamic.global.requirejs(js.Array("./web3.js"),
-//        (nsWeb3: js.Dynamic) =>
-//          println(nsWeb3)
-//          //println(js.Dynamic.global.web3)
-//     )
-
-
-      val add = "0xFcA4C88f2E7C4567c0b7a40E0d5785800CD0C9de"
-      val balance = web3.eth.getBalance(add).toNumber() / 1e18
-      balanceValue() = s"$balance RETH"
-    }
-
-
-    def queryContract = () => {
-      val contract = web3.eth.contract(contractABI).at("0xae59f07cd8cb680db5d265359d6faa841570656b")
-      contactValue() = contract.getNb.call().toString
-    }
-
-//    val uploadWallet = input(`type`:="file", id := "fileInput", accept := ".json").render
-//    val password = input(`type`:="password").render
-
-//    val loadWallet = (e: Event) => {
-//      val reader = new dom.FileReader()
-//      reader.readAsText(uploadWallet.files.item(0))
-//      reader.onloadend =
-//        (e: ProgressEvent) => {
-//          load(reader.result.asInstanceOf[String], password.value)
-//          // v() = read[Wallet](reader.result.asInstanceOf[String]).toString
-//        }
-//    }
-//
-//    uploadWallet.onchange = loadWallet
-//    password.onkeyup = loadWallet
-
-//    dom.document.body.appendChild(div(uploadWallet, password, br(), u(Rx(v()))).render)
-
-    val balance = button("Query Balance", onclick := queryBalance)
-    val contract = button("Query Contract", onclick := queryContract)
-
-    dom.document.body.appendChild(
-      div(
-        balance, br(),
-        "balance: ", Rx(balanceValue()), br(),
-        contract, br(),
-        "contract: ", Rx(contactValue()),
-      ).render
-    )
-
+  @JSGlobal("uportconnect.Connect")
+  @js.native
+  class Connect(appName: String) extends js.Object {
+    def getWeb3(): Web3 = js.native
   }
+
+
+  def main(): Unit = {
+//
+
+    val contractAddress = "0xae59f07cd8cb680db5d265359d6faa841570656b"
+//    val web3 = Var[Option[Web3]] {
+//      None
+//    }
+
+//    def localWeb3 = {
+//      val web3 = js.Dynamic.newInstance(js.Dynamic.global.Web3)().asInstanceOf[Web3]
+//      val provider = js.Dynamic.newInstance(web3.providers.HttpProvider)("http://localhost:8545").asInstanceOf[HttpProvider]
+//      web3.setProvider(provider)
+//      web3
+//    }
+
+    dom.window.addEventListener("load", (e: dom.Event) => {
+
+      // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+      if (js.typeOf(js.Dynamic.global.web3) != "undefined") {
+        startApp(js.Dynamic.global.web3.asInstanceOf[Web3])
+      } else {
+        val uport = new Connect("DataWards")
+        startApp(uport.getWeb3)
+      }
+    })
+
+    def startApp(web3: Web3) {
+
+      val contractABI =
+        js.JSON.parse("""[{"constant":false,"inputs":[],"name":"getNb","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"propose","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"inputs":[],"payable":false,"type":"constructor"}]""")
+
+      val errorValue = Var[Option[Error]] { None }
+      val balanceValue = Var { "NA" }
+      val contactValue = Var { "NA" }
+      val transactionValue = Var[Option[String]] { None }
+
+      def defined[T](t: T) =
+        if(js.isUndefined(t)) None else Some(t)
+
+      def contract: js.Dynamic = web3.eth.contract(contractABI).at(contractAddress)
+
+      def logError[T](error: Error)(f: => T): Unit = {
+        if(error != null) errorValue() = Some(error)
+        else f
+      }
+
+      def queryBalance = () => {
+        web3.eth.getAccounts(querryAccounts)
+
+        def querryAccounts(error: Error, accounts: js.Array[js.Dynamic]) = logError(error) {
+          def add = defined(accounts).flatMap(_.headOption)
+
+          def setBalance(error: js.Error, balance: BigNumber) = logError(error) {
+            balanceValue() = s"${balance.toNumber() / 1e18} RETH"
+          }
+
+          add match {
+            case Some(add) => web3.eth.getBalance(add.toString, callBack = setBalance)
+            case None => errorValue() = Some(Error("Address undefined"))
+          }
+        }
+
+      }
+
+
+      def queryContract = () => {
+        def setNbContract(error: js.Error, result: js.Dynamic) = logError(error) {
+         contactValue() = result.toString
+        }
+
+        contract.getNb.call(setNbContract(_,_))
+      }
+
+      def callPropose = () => {
+        val getData = contract.propose.getData()
+        val transaction =
+          js.Dictionary[js.Any](
+            "to" -> contractAddress,
+            "data" -> getData)
+
+        def proposeCallBack(error: Error, result: String) = logError(error) {
+          transactionValue() = Some(result)
+        }
+
+        web3.eth.sendTransaction(transaction, proposeCallBack(_, _))
+
+        //println(getData)
+        //finally paas this data parameter to send Transaction
+//        web3.eth.sendTransaction({to:address, from:organisationWallet, data: getData});
+//
+//
+//        web3.eth.sendTransaction(contract.propose)
+//        contract.propose()
+//        miniTokentoken.transfer(toAddress, value, { from: addr })
+//
+//          .then(function (txHash) {
+//            console.log('Transaction sent')
+//            console.dir(txHash)
+//
+//            waitForTxToBeMined(txHash)
+//          })
+//
+//        .catch(console.error)
+      }
+
+      //    val uploadWallet = input(`type`:="file", id := "fileInput", accept := ".json").render
+      //    val password = input(`type`:="password").render
+
+      //    val loadWallet = (e: Event) => {
+      //      val reader = new dom.FileReader()
+      //      reader.readAsText(uploadWallet.files.item(0))
+      //      reader.onloadend =
+      //        (e: ProgressEvent) => {
+      //          load(reader.result.asInstanceOf[String], password.value)
+      //          // v() = read[Wallet](reader.result.asInstanceOf[String]).toString
+      //        }
+      //    }
+      //
+      //    uploadWallet.onchange = loadWallet
+      //    password.onkeyup = loadWallet
+
+      //    dom.document.body.appendChild(div(uploadWallet, password, br(), u(Rx(v()))).render)
+
+
+      val balanceButton = button("Query Balance", onclick := queryBalance)
+      val contractButton = button("Query Contract", onclick := queryContract)
+      val proposeButton = button("Propose", onclick := callPropose)
+
+      val errorMessage: Rx[String] = errorValue.map(_.map(_.message).getOrElse("No error"))
+
+      dom.document.body.appendChild(
+        div(
+          balanceButton, "balance: ", Rx(balanceValue()), br(),
+          contractButton, "number of contracts: ", Rx(contactValue()), br(),
+          proposeButton, br(),
+          a(href := "https://rinkeby.etherscan.io/address/" + contractAddress, target := "_blank", "contract transactions"), br(),
+          "Error: ", Rx(errorMessage())
+        ).render
+      )
+
+    }
+  }
+
+
+
+  //    js.Dynamic.global.requirejs(
+  //      js.Array("./ethereumjs-tx"),
+  //      (ns: js.Dynamic) => {
+  //        val rawTx = js.Array(
+  //          "0x00",
+  //          "0x09184e72a000",
+  //          "0x2710",
+  //          "0x0000000000000000000000000000000000000000",
+  //          "0x00",
+  //          "0x7f7465737432000000000000000000000000000000000000000000000000000000600057",
+  //          "0x1c",
+  //          "0x5e1d3a76fbf824220eafc8c79ad578ad2b67d01b0c2425eb1f1347e8f50882ab",
+  //          "0x5bd428537f05f9830e93792f90ea6a3e2d1ee84952dd96edbae9f658f831ab13")
+  //        val tx = js.Dynamic.newInstance(ns.Tx)(rawTx).asInstanceOf[Transaction]
+  //        println(tx.verifySignature())
+  //      }
+  //    )
+
+  //    def load(wallet: String, password: String) = {
+  //      println(js.typeOf(js.Dynamic.global.mist))
+  //      js.Dynamic.global.requirejs(
+  //        js.Array("./ethereumjs-wallet", "ethereumjs-tx"),
+  //        (nsW: js.Dynamic, nsTx: js.Dynamic) => {
+  //          val w = nsW.Wallet.fromV3(wallet, password).asInstanceOf[Wallet]
+  //
+  //          val rawTx = js.Dictionary(
+  //             "nonce" -> "0x00",
+  //             "gasPrice" -> "0x09184e72a000",
+  //             "gasLimit" -> "0x2710",
+  //             "to" -> "0x0000000000000000000000000000000000000000",
+  //             "value" -> "0x00",
+  //             "data" -> "0x7f7465737432000000000000000000000000000000000000000000000000000000600057"
+  //          )
+  //
+  //          val tx = js.Dynamic.newInstance(nsTx.Tx)(rawTx).asInstanceOf[Transaction]
+  //          tx.sign(w.getPrivateKey)
+  //
+  //          println(tx.serialize().mkString)
+  //         // v() = w.getPrivateKey()
+  //        }
+  //      )
+  //    }
 }
